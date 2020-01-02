7c4d7698dc68ffd73efa7709d1ff7f3a1d2ed9f3
Kasper Kristensen <kaskr@dtu.dk>
2020-01-02 15:45:59 +0100

    new script

---
+++ bin/install_and_check_cppad.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
+if [ ! -f ~/.R/Makevars ]; then
+    mkdir -p ~/.R
+    echo "CXX = g++ -Wall -pedantic -Werror" > ~/.R/Makevars
+fi
+
+make cran-version
+sed -i 's/framework=c.*/framework="CppAD",/g' TMB/R/TMB.R
+make cran-check
+make install
+echo "library(TMB);precompile();runExample(all=TRUE)" | R --slave
